85ee6c4a42d7a930b1cd06246127592cc935c352
juned <junedsarwar@gmail.com>
2020-02-05 09:09:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const temp = [20,30,10,73,65,11,21];
+
+const weekdays=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+
+
+for(let i =0;i<temp.length;i++){
+  // console.log(`the tempeeture on ${weekdays[i]} was ${temp[i]}`);
+}
+
+var total =0;
+for(let i =0;i<temp.length;i++){
+ total+=temp[i];
+}
+
+//console.log(total);
