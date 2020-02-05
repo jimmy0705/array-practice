ddbad9188dee46a68f933140d7f9cf8d772ca793
juned <junedsarwar@gmail.com>
2020-02-05 09:29:01 +0000

    checking vowel

--- index.js
+++ index.js
@@ -14,3 +14,58 @@ for(let i =0;i<temp.length;i++){
 }
 
 //console.log(total);
+
+/////////////////////////
+var x1 = ["a","e","i","o","u"];
+var x2 = "abacudido";
+var x3=x2.split("");
+//console.log(x3);
+var arr = [];
+
+//console.log(x1.length);
+// console.log(x1.indexOf("a"));
+
+for(var i=0;i<x1.length;i++){
+  // console.log(x3[i]);
+   for(var j =0;j<x3.length;j++){
+       if(x1[i]== x3[j]){
+           arr.push(x3[j]);
+             x3.splice(j,1);
+
+          
+       }
+       else{
+            //arr.push(x2.charAt(j));
+            // arr.push(x3[j]);
+            //  x3.splice(j,1);
+
+       }
+      
+   }
+  // console.log(arr);
+}
+
+   console.log(arr);
+   var arrJoin=arr.join("");
+   console.log(arrJoin);
+
+
+
+//===================================================//
+//console.log(x3);
+
+// for(var i=0;i<x1.length;i++){
+//   // console.log(x3[i]);
+//    for(var j =0;j<x2.length;j++){
+//        if(x1[i]== x2.charAt(j)){
+//            arr.push(x2.charAt(j));
+//            x2.charAt(j).trim();
+//        }
+//        else{
+//             arr.push(x2.charAt(j));
+
+//        }
+      
+//    }
+  
+// }
